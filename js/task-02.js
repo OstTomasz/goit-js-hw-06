7178979cb5e3d42addc09d61c1f3b6ead8f05b81
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-02-18 15:19:10 +0100

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const createList = (ingredients) => {
+  ingredients.forEach((ingredient) => {
+    const item = document.createElement("li");
+    item.class = "item";
+    item.textContent = ingredient;
+    list.append(item);
+  });
+};
+createList(ingredients);
