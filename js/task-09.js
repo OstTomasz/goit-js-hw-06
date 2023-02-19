634d6396e1e9e22feae1bb100bc2088b0b3f9091
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-02-19 17:05:03 +0100

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
+const changeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+changeColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  color.textContent = document.body.style.backgroundColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
