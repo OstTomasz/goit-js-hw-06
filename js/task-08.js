aa0403f9a2db3290b6b1f6159bebf9fa58b82305
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2023-02-19 16:55:49 +0100

    task 8 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Wszystkie pola powinny zostać wypełnione!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
