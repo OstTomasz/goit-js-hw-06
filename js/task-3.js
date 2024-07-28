384454802fb88c81819d0ca8d15d9c508dd8fc83
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-07-28 16:21:06 +0200

    markup

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+console.log("Zadanie 3");
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+console.log("---------------------");
